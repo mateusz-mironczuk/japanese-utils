04430d4d91c14a1ac22355379375dd4793824208
Mateusz Mirończuk <dev.mironczuk@gmail.com>
2022-03-14 22:56:30 +0100

    Fixed an error when found URL is realtive.

--- src/modules/generate-core-deck/takoboto.js
+++ src/modules/generate-core-deck/takoboto.js
@@ -1,14 +1,14 @@
 import * as cheerio from 'cheerio'
 import nodeFetch from 'node-fetch'
 
+export const indexURL = 'https://takoboto.jp/'
+
 const searchResultHeaderQuery = 'div.ResultDiv'
   + ' > div[style="font-size:22px;padding-bottom:1px;padding-top:3px"]'
 const transliterationContainerQuery = 'div[style="border:1px solid #808080"]'
 const transliterationInKanaQuery = 'span:not([style*="font-size:15px"])'
 const pitchDropStyle = 'border-top:2px solid #FF6020;'
   + 'border-right:2px solid #FF6020'
-
-export const indexURL = 'https://takoboto.jp/'
 
 export async function getPitchPattern(entry) {
   const url = await getEntryURL(entry)
@@ -32,13 +32,16 @@ function trimDifferentiationFromWord(word) {
 
 function findEntryURLInSearchResults(html, wordsToFind) {
   const $ = cheerio.load(html)
-  return $(searchResultHeaderQuery)
+  const url = $(searchResultHeaderQuery)
     .toArray()
     .map(header => $(header))
     .find(header => findProperSearchResultHeader(header, wordsToFind))
     .parent()
     .find('a')
     .attr('href')
+  return url.startsWith(indexURL)
+    ? url
+    : indexURL + url
 }
 
 function findProperSearchResultHeader(header, wordsToFind) {
