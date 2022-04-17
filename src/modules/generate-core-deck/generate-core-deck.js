1aba4e05d988a9a01c151717e1913ca75805a4a7
Mateusz Mirończuk <dev.mironczuk@gmail.com>
2022-04-18 01:39:03 +0200

    Renamed inconsistent variable name.

--- src/modules/generate-core-deck/generate-core-deck.js
+++ src/modules/generate-core-deck/generate-core-deck.js
@@ -1,4 +1,4 @@
-import * as downloadDeck from './downloaded-deck.js'
+import * as downloadedDeck from './downloaded-deck.js'
 import * as entriesDownloader from './entries-downloader.js'
 import fsPromises from 'fs/promises'
 import * as iknow from './iknow.js'
@@ -9,7 +9,7 @@ export default async function generateCoreDeck(core, directoryPath) {
   const filePath = path.join(directoryPath, `core ${core}.csv`)
   console.log('Downloading...')
   const iknowEntries = await iknow.getEntries(core)
-  const downloadedEntries = await downloadDeck.getEntries(filePath)
+  const downloadedEntries = await downloadedDeck.getEntries(filePath)
   const deck = { core, directoryPath, filePath, downloadedEntries }
   const filtered = filterDownloadedEntries(deck, iknowEntries)
   await entriesDownloader.download(deck, filtered)
