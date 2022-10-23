b10bfee07f16d1d22fc02a5702e9438dfbeb5d28
Mateusz Mirończuk <dev.mironczuk@gmail.com>
2022-10-23 20:40:49 +0200

    Added a script to generate a kanji deck.

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Various utilities to prepare data for apps which help me learn Japanese.
 ## Utilities
 The repository contains following utility scripts:
 - [generate-core-deck](#generate-core-deck)
+- [generate-kanji-deck](#generate-kanji-deck)
 - [generate-examples-deck](#generate-examples-deck)
 - [generate-kanji-files](#generate-kanji-files)
 - [filter-kanji-svg](#filter-kanji-svg)
@@ -18,6 +19,17 @@ Generates a deck based on [iKnow.jp](https://iknow.jp/content/japanese) and
     - `<core>` is a number describing an iKnow.jp lesson.
 2. The generated deck will be saved in a `decks` directory inside a current
     working directory.
+
+### generate-kanji-deck
+Generates an Anki deck to learn kanji.
+
+#### Usage
+1. Obtain a legal [kradfile](http://www.edrdg.org/wiki/index.php/KANJIDIC_Project)
+    file and a legal [kanjidic2.xml](http://www.edrdg.org/wiki/index.php/KANJIDIC_Project)
+    file.
+2. Run `node src/generate-kanji-deck.js <a path to the kanjidic2.xml> <a path to the kradfile>`.
+3. The generated file will be saved as  `_kanji-deck.csv` in a current working
+    directory.
 
 ### generate-examples-deck
 Generates an examples deck based on [iKnow.jp](https://iknow.jp/content/japanese).
---
+++ src/generate-kanji-deck.js
@@ -0,0 +1,90 @@
+import encodingJapanese from 'encoding-japanese'
+import fs from 'fs/promises'
+import rtkKanjiList from './modules/rtk-kanji-list.js'
+import { XMLParser } from 'fast-xml-parser'
+
+const kanjisDictionary = process.argv[2]
+const kanjisDecompositions = process.argv[3]
+
+if (!kanjisDictionary || !kanjisDecompositions) {
+  console.error(
+    'You have to specify a kanjidic2.xml and kradfile files to process.'
+  )
+} else {
+  await run()
+}
+
+async function run() {
+  const decompositions = await readDecompositions()
+  const kanjis = await readKanjis()
+  const csv = generateCSV(kanjis, decompositions)
+  await fs.writeFile('_kanji-deck.csv', csv, 'utf-8')
+}
+
+async function readKanjis() {
+  const contents = await fs.readFile(kanjisDictionary, 'utf-8')
+  const cleared = removeUnsupportedDoctype(contents)
+  return generateKanjisList(cleared)
+}
+
+function removeUnsupportedDoctype(contents) {
+  const start = contents.indexOf('<kanjidic2>')
+  return contents.substring(start)
+}
+
+function generateKanjisList(contents) {
+  return new XMLParser({ ignoreAttributes: false })
+    .parse(contents)
+    .kanjidic2.character
+    .filter(({ literal }) => literal in rtkKanjiList)
+    .map(simplify)
+    .sort((a, b) => a.id - b.id)
+}
+
+function simplify(entry) {
+  return {
+    id: rtkKanjiList[entry.literal],
+    kanji: entry.literal,
+    meaning: getMeaning(entry),
+  }
+}
+
+function getMeaning(entry) {
+  return Array
+    .of(entry.reading_meaning.rmgroup.meaning)
+    .flat()
+    .filter(item => typeof item === 'string')[0]
+}
+
+async function readDecompositions() {
+  const contents = await fs.readFile(kanjisDecompositions)
+  const decodedBuffer = encodingJapanese.convert(contents, {
+    from: 'EUCJP', to: 'UNICODE'
+  })
+  const decoded = encodingJapanese.codeToString(decodedBuffer)
+  return parseDecompositions(decoded)
+}
+
+function parseDecompositions(contents) {
+  return contents
+    .split('\n')
+    .reduce((decompositions, line) => {
+      const [kanji, radicals] = line.split(' : ')
+
+      if (!line.startsWith('#')) {
+        decompositions[kanji] = radicals
+      }
+
+      return decompositions
+    }, {})
+}
+
+function generateCSV(kanjis, decompositions) {
+  return kanjis
+    .map((entry) => generateCSVEntry(entry, decompositions))
+    .join('\n')
+}
+
+function generateCSVEntry(entry, decompositions) {
+  return `${entry.kanji};${entry.meaning};${decompositions[entry.kanji]}`
+}
